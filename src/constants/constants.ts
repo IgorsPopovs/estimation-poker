74c4aedffd2619a1462e9a414801ada6553dce81
faq322 <34307377+faq322@users.noreply.github.com>
2023-05-10 12:35:11 +0300

    first deployment to heroku

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'estimation-poker-server.surge.sh';//'http://localhost:5000';
 
 export const BASE_API_URL = BASE_URL + '/api';
 
