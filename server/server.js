252bee499210613b39d2d71a1a835e36bd6fe3be
faq322 <34307377+faq322@users.noreply.github.com>
2023-05-10 15:09:23 +0300

    Add client build

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -86,4 +87,11 @@ server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
